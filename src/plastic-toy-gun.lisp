7f447ff1ffaaf495e9439ae7ade1aafeaee4a5ab
mocchi <boom.boom.planet@gmail.com>
2015-12-31 22:21:05 +0900

    fix logger

--- src/plastic-toy-gun.lisp
+++ src/plastic-toy-gun.lisp
@@ -1,17 +1,19 @@
 (in-package :cl-user)
-(defpackage toy-gun
+(defpackage plastic-toy-gun
   (:use :cl)
   (:use :usocket)
   (:use :bordeaux-threads)
   (:export :start
            :dispose
            :*log*
+           :*debug-log*
            :*cartridge*
            :make-server))
 
-(in-package :toy-gun)
+(in-package :plastic-toy-gun)
 
 (defparameter *log* t)
+(defparameter *debug-log* t)
 (defparameter *cartridge* '())
 
 (defun make-server (&key (port 8080) (address "localhost"))
@@ -21,19 +23,19 @@
   (usocket:socket-accept server))
 
 (defun dispose (server)
-  (format *log* "dispose server~%")
+  (format *debug-log* "dispose server~%")
   (usocket:socket-close server))
 
 (defun handler (client)
   (handler-case
    (with-open-stream (stream (usocket:socket-stream client))
                      (funcall *cartridge* stream))
-   (error (c) (format t "~%dump error: ~a~%" c))))
+   (error (c) (format *debug-log* "~%dump error: ~a~%" c))))
 
 (defun start (server)
   (unless *cartridge*
-    (error "(setq toy-gun:*cartrige* #'your-ink)~%"))
-  (format *log* "start server~%")
+    (error "(setq plastic-toy-gun:*cartrige* #'your-ink)~%"))
+  (format *debug-log* "start server~%")
   (let ((sock '()))
     (unwind-protect
         (loop (setq sock (accept server))
